0c7dcbc90f8a709ec27b98bcf5395add8a494d75
Pelayogm <pelagrmenendez12@gmail.com>
2025-01-23 00:17:16 +0100

    Barra de Navegacion V0.5.5

--- src/BarraNavegacion.jsx
+++ src/BarraNavegacion.jsx
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './BarraNavegacion.css'
-import Maquinas from './Maquinas';
 
 function BarraDeNavegacion() {
 
