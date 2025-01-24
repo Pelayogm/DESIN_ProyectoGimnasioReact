07c76e24bdf68ca0c670eb285aa8093acfb6edfb
Pelayogm <pelagrmenendez12@gmail.com>
2025-01-24 19:51:47 +0100

    Cambios y comprobacion

--- src/Natacion.js
+++ src/Natacion.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BarraDeNavegacion from "./BarraNavegacion";
+import Footer from "./Footer";
 
 const Natacion = () => {
   const [datos, setDatos] = useState(null);
